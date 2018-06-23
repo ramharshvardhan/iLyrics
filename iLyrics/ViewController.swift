fb3dd6ae835ebb5ce9f16bb06bf435ae57bb6296
Ram Harshvardhan Radhakrishnan <ramharshvardhan@gmail.com>
2018-06-23 13:38:01 -0500

    Initial Commit

---
+++ iLyrics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iLyrics
+//
+//  Created by Ram Harshvardhan Radhakrishnan on 6/23/18.
+//  Copyright © 2018 Ram Harshvardhan Radhakrishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
